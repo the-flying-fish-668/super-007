39bfcdeecab61460a738987884a65e0ffb552191
the-flying-fish-668 <89724825+the-flying-fish-668@users.noreply.github.com>
2021-08-29 16:27:54 +0800

    Add files via upload

---
+++ ballclock/linkqueue.h
@@ -0,0 +1,18 @@
+typedef int datatype;
+
+typedef struct node {
+	datatype data;
+	struct node *next;
+}listnode , *linklist;
+
+typedef struct {
+	linklist front;
+	linklist rear;
+}linkqueue;
+
+linkqueue * queue_create();
+int enqueue(linkqueue *lq, datatype x);
+datatype dequeue(linkqueue *lq);
+int queue_empty(linkqueue *lq);
+int queue_clear(linkqueue *lq);
+linkqueue * queue_free(linkqueue *lq);
